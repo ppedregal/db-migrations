26a91c2d54093d6f6b3ec04c97323ef84e22a43a
Pablo Pedregal <pablo.pedregal@vass.es>
2020-08-10 17:10:13 +0200

    initial version

---
+++ flyway/src/test/java/dbmigrations/flyway/FlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package dbmigrations.flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
